315499f918f6564a150b96885d3891c6f373f4ba
ana <ana.bonaca@yale.edu>
2014-10-07 14:06:02 -0400

    smoothed options, psf related calls

--- psp.py
+++ psp.py
@@ -1,73 +1,51 @@
 from __future__ import print_function, division
 
-from math import *
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-from matplotlib.patches import Circle
-from matplotlib.collections import PatchCollection
-
-import astropy
-from astropy.io import fits
-from astropy import wcs
-from astropy import units as u
-from astropy.coordinates import ICRS
-from astropy.coordinates.angles import Angle, Longitude
-from astropy.table import Table
-
-import scipy.stats
-from scipy.optimize import leastsq
-from scipy.spatial import cKDTree
-
-import random
-import time
-import glob
 import os
+from shutil import copy
 import subprocess
 from subprocess import call, Popen, PIPE
-from myutils import *
-
-from collections import Counter
 import string
-import threading
+
+import numpy as np
 
 pi=3.14159265359
 
+# define options keys and default values for 4 types of options files
+OPTS_DEFAULT = {'daophot': {'read_noise': 6.70, 'gain': 4.20, 'low_good': 7.00, 'high_good': 32766.50, 'fwhm': 2.50, 'sigma_th': 4.00, 'low_sharp': 0.30, 'high_sharp':1.40, 'low_round': -1.00, 'high_round':1.00, 'watch':0.00, 'r_fit': 15.00, 'r_psf': 20.00, 'var_psf': 2.00, 'sky': 0.00, 'psf_model': 5.00, 'psf_clean': 5.00, 'use_sat': 0.00, 'err_percent': 0.75, 'err_profile': 5.00},
+'photo': {'a1': 3, 'a2': 4, 'a3': 5, 'a4': 6, 'a5': 7, 'a6': 8, 'a7': 10, 'a8': 11, 'a9': 12, 'aa': 13, 'ab': 14, 'ac': 15, 'inner_sky': 20., 'outer_sky': 35.},
+'allstar': {'r_fit': 15.00, 'clip_exp': 6.00, 'redet_centroid': 1.00, 'clip_range': 2.50, 'watch': 0.00, 'group_max': 50.00, 'err_percent': 0.75, 'err_profile': 5.00, 'inner_sky': 20., 'outer_sky': 35.},
+'allframe': {'clip_exp': 6.00, 'clip_range': 2.50, 'geo_coeff': 6.00, 'watch': 0.00, 'iter_min': 5, 'iter_max': 200, 'err_percent': 0.75, 'err_profile': 5.00, 'inner_sky': 20., 'outer_sky': 35.}}
+
+# map option keys to daophot parameters
+OPTS_KEYS = {'daophot': {'read_noise': 'READ NOISE (ADU; 1 frame)', 'gain': 'GAIN (e-/ADU; 1 frame)', 'low_good': 'LOW GOOD DATUM (in sigmas)', 'high_good': 'HIGH GOOD DATUM (in ADU)', 'fwhm': 'FWHM OF OBJECT', 'sigma_th': 'THRESHOLD (in sigmas)', 'low_sharp': 'LS (LOW SHARPNESS CUTOFF)', 'high_sharp': 'HS (HIGH SHARPNESS CUTOFF)', 'low_round': 'LR (LOW ROUNDNESS CUTOFF)', 'high_round': 'HR (HIGH ROUNDNESS CUTOFF)', 'watch': 'WATCH PROGRESS', 'r_fit': 'FITTING RADIUS', 'r_psf': 'PSF RADIUS', 'var_psf': 'VARIABLE PSF', 'sky': 'SKY ESTIMATOR', 'psf_model': 'ANALYTIC MODEL PSF', 'psf_clean': 'EXTRA PSF CLEANING PASSES', 'use_sat': 'USE SATURATED PSF STARS', 'err_percent': 'PERCENT ERROR (in %)', 'err_profile': 'PROFILE ERROR (in %)'},
+'photo': {'a1': 'A1', 'a2': 'A2', 'a3': 'A3', 'a4': 'A4', 'a5': 'A5', 'a6': 'A6', 'a7': 'A7', 'a8': 'A8', 'a9': 'A9', 'aa': 'AA', 'ab': 'AB', 'ac': 'AC', 'inner_sky': 'IS', 'outer_sky': 'OS'},
+'allstar': {'r_fit': 'FITTING RADIUS', 'clip_exp': 'CE (CLIPPING EXPONENT)', 'redet_centroid': 'REDETERMINE CENTROIDS', 'clip_range': 'CR (CLIPPING RANGE)', 'watch': 'WATCH PROGRESS', 'group_max': 'MAXIMUM GROUP SIZE', 'err_percent': 'PERCENT ERROR', 'err_profile': 'PROFILE ERROR', 'inner_sky': 'IS', 'outer_sky': 'OS'},
+'allframe': {'clip_exp': 'CE (CLIPPING EXPONENT)', 'clip_range': 'CR (CLIPPING RANGE)', 'geo_coeff': 6.00, 'watch': 'WATCH PROGRESS', 'iter_min': 'MINIMUM ITERATIONS', 'iter_max': 'MAXIMUM ITERATIONS', 'err_percent': 'PERCENT ERROR', 'err_profile': 'PROFILE ERROR', 'inner_sky': 'IS', 'outer_sky': 'OS'}}
+
 class Dao(object):
-	def __init__(self, fname, cmd, opts):
+	def __init__(self, cmd, opts):
 		"""Initialization for Dao objects"""
-		
-		# path to image
-		self.fname = fname
-		
-		# root name
-		self.name = fname[:fname.rfind('.')]
-		
-		# working directory
-		sidx = self.name.rfind('/')
-		if sidx == -1:
-			self.dr = ""
-		else:
-			self.dr = self.name[:sidx]
 		
 		# if which cmd not found -> go install, else
 		self.cmd = cmd
 		
 		# options
 		self.opts = opts
+	
+	def init_names(self, fname):
+		# path to image
+		self.fname = fname
 		
-		# define options keys and default values for 4 types of options files
-		self.opkeys = {'daophot': {'re': 6.70, 'ga': 4.20, 'lo': 7.00, 'hi': 32766.50, 'fw': 2.50, 'th': 4.00, 'ls': 0.30, 'hs':1.40, 'lr': -1.00, 'hr':1.00, 'wa':0.00, 'fi': 15.00, 'ps': 20.00, 'va': 2.00, 'sk': 0.00, 'an': 5.00, 'ex': 5.00, 'us': 0.00, 'pe': 0.75, 'pr': 5.00},
-		'photo': {'a1': 3, 'a2': 4, 'a3': 5, 'a4': 6, 'a5': 7, 'a6': 8, 'a7': 10, 'a8': 11, 'a9': 12, 'aa': 13, 'ab': 14, 'ac': 15, 'is': 20., 'os': 35.},
-		'allstar': {'fi': 15.00, 'ce': 6.00, 're': 1.00, 'cr': 2.50, 'wa': 0.00, 'ma': 50.00, 'pe': 0.75, 'pr': 5.00, 'is': 20., 'os': 35.},
-		'allframe': {'ce': 6.00, 'cr': 2.50, 'ge': 6.00, 'wa': 0.00, 'mi': 5, 'ma': 200, 'pe': 0.75, 'pr': 5.00, 'is': 20., 'os': 35.}}
+		# root name 
+		self.name = os.path.splitext(fname)[0]
 		
+		# working directory
+		self.dr = os.path.split(fname)[0]
 	
 	def write_opts(self, op_type, pars={}, optname=None, general_name=False):
 		"""Write options file"""
-		if op_type not in self.opkeys.keys():
-			print("options type needs to be one of the following: {0}".format(self.opkeys.keys()))
+		if op_type not in OPTS_KEYS.keys():
+			print("options type needs to be one of the following: {0}".format(OPTS_KEYS.keys()))
 			return
 		
 		if general_name:
@@ -79,18 +57,18 @@ class Dao(object):
 			self.optname = optname
 		
 		# check which parameters remain default
-		dkeys = [x for x in self.opkeys[op_type].keys() if x not in pars.keys()]
+		dkeys = [x for x in OPTS_KEYS[op_type].keys() if x not in pars.keys()]
 		
 		# write options file
 		f = open(self.optname, 'w')
 		
 		# input parameters
 		for k in pars.keys():
-			f.write("{0} = {1}\n".format(k, pars[k]))
+			f.write("{0} = {1}\n".format(OPTS_KEYS[op_type][k], pars[k]))
 		
 		# default parameters
 		for k in dkeys:
-			f.write("{0} = {1}\n".format(k, self.opkeys[op_type][k]))
+			f.write("{0} = {1}\n".format(OPTS_KEYS[op_type][k], OPTS_DEFAULT[op_type][k]))
 		
 		f.close()
 	
@@ -112,15 +90,28 @@ class Dao(object):
 		#print("err: ", stderrdata)
 		
 		# close process
-		if p.poll==None:
+		# check p.returncode for !=0, raise error, check daophot log, stop here
+		if p.poll()==None:
 			p.terminate()
 		
+		return stdoutdata
+	
+	def find_instream(self, stream, search_string):
+		"""Return index in stream where search_string ends"""
+		ind = string.find(stream, search_string)
+		s1 = ind + len(search_string)
+		
+		return s1
 		
 class Find(Dao):
-	def __init__(self, img, opts):
-		super(Find, self).__init__(img, 'daophot', opts)
+	def __init__(self, opts):
+		super(Find, self).__init__('daophot', opts)
 		
-	def doit(self):
+	def __call__(self, fname):
+		self.init_names(fname)
+		self.write_opts('daophot', general_name=True)
+		self.write_opts('daophot', pars=self.opts['daophot'])
+		
 		"""Call daophot and run find command"""
 		
 		# build call
@@ -135,10 +126,107 @@ find
 y
 
 exit
-END_DAOPHOT""".format(self.optname, self.opts['stacknum'], self.name)
+END_DAOPHOT""".format(self.optname, self.opts['misc']['stacknum'], self.name)
+		
+		# call daophot find
+		stdout = self.run_cmd(lines, args=["<<", "END_DAOPHOT"])
+		
+		search_string = "Sky mode and standard deviation = "
+		s1 = self.find_instream(stdout, search_string)
+		self.opts['misc']['sky_mode'] = float(stdout[s1:s1+8])
+
+class Aper(Dao):
+	def __init__(self, opts):
+		super(Aper, self).__init__('daophot', opts)
+		
+	def __call__(self, fname):
+		"""Call daophot and run find command"""
+		self.init_names(fname)
+		self.write_opts('photo', self.opts['photo'], general_name=True)
+		self.write_opts('daophot', general_name=True)
+		self.write_opts('daophot', self.opts['daophot'])
+		
+		# build call
+		lines="""options
+{0}
+
+attach {1}.fits
+
+photometry
+
+
+{1}.coo
+{1}.ap
+
+exit
+END_DAOPHOT""".format(self.optname, self.name)
+		
+		# call daophot aperture photometry
+		stdout = self.run_cmd(lines, args=["<<", "END_DAOPHOT"])
+		
+		search_string = "Estimated magnitude limit (Aperture 1): "
+		s1 = self.find_instream(stdout, search_string)
+		self.opts['misc']['maglim'] = float(stdout[s1:s1+4])
+
+class PickStars(Dao):
+	def __init__(self, opts):
+		super(PickStars, self).__init__('daophot', opts)
+		
+	def __call__(self, fname):
+		"""Call daophot and run find command"""
+		self.init_names(fname)
+		self.write_opts('daophot', general_name=True)
+		self.write_opts('daophot', pars=self.opts['daophot'])
+		
+		# build call
+		lines="""options
+{0}
+
+attach {1}.fits
+
+pickpsf
+{1}.ap
+100,{2}
+{1}.lst1
+
+exit
+END_DAOPHOT""".format(self.optname, self.name, self.opts['misc']['maglim'])
 		
 		# call daophot find
 		self.run_cmd(lines, args=["<<", "END_DAOPHOT"])
+		
+class GetPSF(Dao):
+	def __init__(self, opts):
+		super(GetPSF, self).__init__('daophot', opts)
+		
+	def __call__(self, fname):
+		"""Call daophot and run find command"""
+		self.init_names(fname)
+		self.write_opts('daophot', general_name=True)
+		self.write_opts('daophot', pars=self.opts['daophot'])
+		
+		# build call
+		lines="""options
+{0}
+
+attach {1}.fits
+
+psf
+{1}.ap
+{1}.lst1
+{1}.psf
+
+exit
+END_DAOPHOT""".format(self.optname, self.name)
+		
+		# call daophot find
+		self.run_cmd(lines, args=["<<", "END_DAOPHOT"])
+
+# general helper functions
+def cleanup_files(fname, extensions):
+	"""Remove files with root fname and extensions"""
+	for ext in extensions:
+		silent_remove(os.path.splitext(fname)[0]+".{0}".format(ext))
 
 def silent_remove(fname):
 	"""Remove file if exists"""
@@ -146,22 +234,77 @@ def silent_remove(fname):
 		os.remove(fname)
 	except OSError:
 		pass
-	
-def test():
-	fname = "test.fits"
-	cmd = "daophot"
-	opts = {'rpsf': 20, 'rfit': 15, 'stacknum': 1}
-	f = Find(fname, opts)
-	print(f.name, f.dr)
-	
-	# cleanup
-	for ext in ("log", "coo"):
-		silent_remove(f.name+".{0}".format(ext))
-	
-	f.write_opts('daophot', general_name=True)
-	f.write_opts('daophot', pars={'ps':f.opts['rpsf'], 'fi':f.opts['rfit']})
-	f.doit()
 
+def set_indopts(fname, opts):
+	"""Read gain, readnoise and saturation level for input fname and store to opts"""
+	
+	info = os.path.splitext(fname)[0]+".info"
+	keys = ('gain', 'read_noise', 'high_good')
+	
+	if os.path.isfile(info)==False:
+		print("Info file not found, proceeding with defaults.")
+		for i, key in enumerate(keys):
+			opts['daophot'][key] = OPTS_DEFAULT['daophot'][key]
+	else:
+		op = np.loadtxt(info)
+		for i, key in enumerate(keys):
+			opts['daophot'][key] = op[i]
+
+def test():
+	"""Testing new photometric pipeline"""
+	
+	# global options
+	opts = {'daophot': {'r_psf': 20, 'r_fit': 15}, 'photo': {}, 'allstar': {}, 'allframe': {}, 'misc': {'stacknum': 1, 'counts_limit': 15000, 'number_limit': 400, 'sigma_psf': 4.5, 'sigma_all': 2.5}}
+	
+	# image
+	fname = "test.fits"
+	
+	# individual options
+	set_indopts(fname, opts)
+	
+	# psf image (masked image)
+	os.system("python psf_mask.py {0} {1} {2} {3}".format(fname, opts['misc']['counts_limit'], opts['misc']['number_limit'], opts['daophot']['high_good']+1))
+	psfname = os.path.splitext(fname)[0]+"_psf.fits"
+	# if psf image not created, copy the original image
+	if os.path.isfile(psfname)==False:
+		copy(fname, psfname)
+		
+	# cleanup
+	cleanup_files(fname, ("log", "coo"))
+	cleanup_files(psfname, ("log", "coo"))
+	
+	# create photometry objects
+	f = Find(opts)
+	aper = Aper(opts)
+	pick = PickStars(opts)
+	psf = GetPSF(opts)
+	
+	# initial find, to get fwhm
+	f(fname)
+	aper(fname)
+	
+	# once initial find run, determine fwhm, update it in opts
+	
+	print(opts)
+	
+	# options for psf finding
+	opts['daophot']['sigma_th']=opts['misc']['sigma_psf']
+	
+	f(psfname)
+	aper(psfname)
+	pick(psfname)
+	psf(psfname)
+	
+	# print plog
+	
+	# identify psf stars that have bad pixels and remove them from list
+	
+	# check none left
+	
+	
+	
+	
+	
 	
 	
 
